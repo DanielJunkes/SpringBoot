6e2467327c240d18dc6e4e79706beabee5e1cc0a
DanielJunkes <danielmafioleti2@gmail.com>
2024-02-20 16:17:41 -0300

    OrderItem criado sendo um chave primaria composta

--- src/main/java/com/tutorialSpring/AprendendoSpring/config/TestConfig.java
+++ src/main/java/com/tutorialSpring/AprendendoSpring/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.tutorialSpring.AprendendoSpring.entities.Category;
 import com.tutorialSpring.AprendendoSpring.entities.Order;
+import com.tutorialSpring.AprendendoSpring.entities.OrderItem;
 import com.tutorialSpring.AprendendoSpring.entities.Product;
 import com.tutorialSpring.AprendendoSpring.entities.User;
 import com.tutorialSpring.AprendendoSpring.entities.enums.OrderStatus;
 import com.tutorialSpring.AprendendoSpring.repositories.CategoryRepository;
+import com.tutorialSpring.AprendendoSpring.repositories.OrderItemRepository;
 import com.tutorialSpring.AprendendoSpring.repositories.OrderRepository;
 import com.tutorialSpring.AprendendoSpring.repositories.ProductRepository;
 import com.tutorialSpring.AprendendoSpring.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,5 +73,13 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
 	}
 }
